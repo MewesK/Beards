e0dcf09c62869d99313bae0341aae46f65644a64
Mewes Kochheim <mewes@kochheim.de>
2014-06-29 15:12:19 +0200

    More proxy stuff

--- src/main/java/net/mewk/beards/Beards.java
+++ src/main/java/net/mewk/beards/Beards.java
@@ -1,14 +1,19 @@
 package net.mewk.beards;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.mewk.beards.proxy.IProxy;
 
 @Mod(modid = "Beards", name = "Beards", version = "1.7.2-0.1")
 public class Beards {
     @Mod.Instance("Beards")
     public static Beards instance;
+
+    @SidedProxy(clientSide = "net.mewk.beards.proxy.ClientProxy", serverSide = "net.mewk.beards.proxy.ServerProxy")
+    public static IProxy proxy;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
--- src/main/java/net/mewk/beards/proxy/CommonProxy.java
+++ src/main/java/net/mewk/beards/proxy/CommonProxy.java
@@ -1,4 +1,4 @@
 package net.mewk.beards.proxy;
 
-public class CommonProxy implements IProxy {
+public abstract class CommonProxy implements IProxy {
 }
