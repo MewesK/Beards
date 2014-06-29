7a185c9399d2d498ce05063520078d3828a98d28
Mewes Kochheim <mewes@kochheim.de>
2014-06-29 14:42:56 +0200

    Main mod class

---
+++ src/main/java/net/mewk/beards/Beards.java
@@ -0,0 +1,27 @@
+package net.mewk.beards;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "Beards", name = "Beards", version = "1.7.2-0.1")
+public class Beards {
+    @Mod.Instance("Beards")
+    public static Beards instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
